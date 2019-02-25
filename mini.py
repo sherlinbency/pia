43e6a171a0a2b167339b6624f669551c0ebeab4a
sherlinbency <45781622+sherlinbency@users.noreply.github.com>
2019-02-25 14:08:49 +0530

    Create mini.py

---
+++ mini.py
@@ -0,0 +1,23 @@
+void Nminelements(vector<int>list1, int N) 
+{ 
+int n = list1.size(); 
+for (int i = 0; i < N; i++)  
+{ 
+  int min1 = 9999999; 
+  for (int j = 0; j < n; j++) 
+    { 
+      if (list1[j] < min1) 
+      min1 = list1[j]; 
+        } 
+for(int i = 0; i < final_list.size(); i++) 
+  } 
+ int main() 
+{ 
+ vector<int> list1 = {4, 78, 34, 10,  
+                         8, 21, 11, 231};
+  int N = 2; 
+    Nminelements(list1, N); 
+   }
+   }int N
+  
+
